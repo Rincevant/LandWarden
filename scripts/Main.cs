7e1c0e90e2bd48909d8f5310b3090d7558537313
Julien Perona <julien.perona.ext@ac-aix-marseille.fr>
2022-05-24 14:25:06 +0200

    save and load

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public class Main : Node2D
@@ -15,5 +16,35 @@ public class Main : Node2D
     public void _on_Quit_pressed() 
     {
         GetTree().Quit();
+    }
+
+    private string SAVE_DIR = "res://saves/";
+    private string save_path = "res://saves/save.dat";
+
+
+    public void _on_Save_pressed()
+    {
+	    var saveData = new Godot.Collections.Dictionary<string, object>()
+        {
+            { "Score", "123" },
+            { "test" , "julien"}
+        };
+        var saveGame = new File();
+        saveGame.OpenEncryptedWithPass("user://savegame.save", File.ModeFlags.Write, "mypass");
+        saveGame.StoreLine(JSON.Print(saveData));
+        saveGame.Close();
+    }
+
+    public void _on_Load_pressed() 
+    {
+        var saveGame = new File();
+        if (!saveGame.FileExists("user://savegame.save"))return; 
+        saveGame.OpenEncryptedWithPass("user://savegame.save", File.ModeFlags.Read, "mypass");        
+           
+        var saveData = new Godot.Collections.Dictionary<string, object>((Godot.Collections.Dictionary)JSON.Parse(saveGame.GetLine()).Result);
+        var newScore = saveData["Score"].ToString();
+        var testValue = saveData["test"].ToString();            
+       
+        saveGame.Close();
     }
 }
